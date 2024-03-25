8f1993eb10fa0a52c063cf255d2fb55bf92dbaab
Richard Garside <code@nogginbox.co.uk>
2024-03-25 08:35:44 +0000

    Test first = broken test for BlockRule

--- Nogginbox.MailForwarder.Tests/IsExpectedRecipientMailBoxTests.cs
+++ Nogginbox.MailForwarder.Tests/IsExpectedRecipientMailBoxTests.cs
@@ -1,4 +1,5 @@
 ﻿using DnsClient;
+using MimeKit;
 using Nogginbox.MailForwarder.Server.MailboxFilters;
 using Nogginbox.MailForwarder.Server.MailRules;
 using NSubstitute;
@@ -11,6 +12,8 @@ namespace Nogginbox.MailForwarder.Tests;
 
 public class IsExpectedRecipientMailBoxTests
 {
+    const string Tld = "nogginbox.co.uk";
+
     [Fact]
     public async Task NoRulesWillNotDeliverTest()
     {
@@ -21,8 +24,62 @@ public class IsExpectedRecipientMailBoxTests
         var rules = new List<IMessageRule>();
         var filter = new IsExpectedRecipientMailboxFilter(rules, log);
 
-        var to = new Mailbox("code@nogginbox.co.uk");
-        var from = new Mailbox("do-not-reply@nogginbox.co.uk");
+        var to = new Mailbox($"code@null.{Tld}");
+        var from = new Mailbox($"do-not-reply@null.{Tld}");
+
+        // Act
+        var result = await filter.CanDeliverToAsync(sessionContext, to, from, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(MailboxFilterResult.NoPermanently, result);
+    }
+
+    [Theory]
+    [InlineData($"code@null1.{Tld}")]
+    [InlineData($"richard.g@null2.{Tld}")]
+    public async Task AddressesMatchingForwardRulesWillDeliverTest(string email)
+    {
+        // Arrange
+        var sessionContext = Substitute.For<ISessionContext>();
+        var log = Substitute.For<Logging.ILogger>();
+
+        var forwardRecipient = new MailboxAddress("Tester 1", $"tester1@null.{Tld}");
+        var rules = new List<IMessageRule>
+        {
+            new ForwardRule($"*@null1.{Tld}", forwardRecipient),
+            new ForwardRule($"richard.*@null2.{Tld}", forwardRecipient)
+        };
+        var filter = new IsExpectedRecipientMailboxFilter(rules, log);
+
+        var to = new Mailbox(email);
+        var from = new Mailbox($"do-not-reply@null.{Tld}");
+
+        // Act
+        var result = await filter.CanDeliverToAsync(sessionContext, to, from, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(MailboxFilterResult.Yes, result);
+    }
+
+    [Theory]
+    [InlineData($"richard.spam@null.{Tld}")]
+    [InlineData($"richard.leaked@null.{Tld}")]
+    public async Task AddressesMatchingBlockRulesWillNotDeliverTest(string email)
+    {
+        // Arrange
+        var sessionContext = Substitute.For<ISessionContext>();
+        var log = Substitute.For<Logging.ILogger>();
+
+        var forwardRecipient = new MailboxAddress("Tester 1", $"tester1@null.{Tld}");
+        var rules = new List<IMessageRule>
+    {
+        new ForwardRule($"*@null.{Tld}", forwardRecipient),
+        new BlockRule($"richard.*@null.{Tld}")
+    };
+        var filter = new IsExpectedRecipientMailboxFilter(rules, log);
+
+        var to = new Mailbox(email);
+        var from = new Mailbox($"do-not-reply@null.{Tld}");
 
         // Act
         var result = await filter.CanDeliverToAsync(sessionContext, to, from, CancellationToken.None);
